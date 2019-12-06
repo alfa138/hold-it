25fd161a1ca2e630e63717fd618e4596ad2f0794
avenger <munioz.eitan@gmail.com>
2019-12-06 20:17:47 +0200

    finished demo !

--- index.js
+++ index.js
@@ -9,15 +9,29 @@ let pink = document.getElementById('pink');
 let black = document.getElementById('black');
 let sub = document.getElementById('sub');
 
-let start, end;
+// old ******************************
+
+let start, end, interval, intervalCounter = 0;
+
 red.addEventListener('mousedown', e => {
     start = new Date();
-    console.log('time', start);
+    console.log('mousedown', start);
+
+    interval = setInterval(() => {
+        intervalCounter++;
+        if (intervalCounter >= 1) {
+            console.log('>= 2');
+            sub.classList.remove('none');
+            clearInterval(interval);
+        }
+
+    }, 1000);
 });
 
 red.addEventListener('mouseup', e => {
     end = new Date();
     console.log('mouseUp', end);
+    clearInterval(interval);
 
     const dif = start.getTime() - end.getTime();
 
@@ -25,13 +39,27 @@ red.addEventListener('mouseup', e => {
     const Seconds_Between_Dates = Math.abs(Seconds_from_T1_to_T2);
     console.log(Seconds_Between_Dates);
 
-    if (Seconds_Between_Dates > 2) {
-        sub.classList.remove('none');
-    } else {
+    if (Seconds_Between_Dates < 1) {
         color.style.backgroundColor = 'red';
-        sub.classList.add('none');
     }
+
 });
+
+
+// new *****************************
+
+let inVal, counter = 0;
+
+// red.addEventListener('mousedown', () => {
+//     inVal = setInterval(() => {
+//         counter++;
+//         if (counter >= 2) {
+//             console.log('counter > 2');
+//         }
+//     }, 1000);
+//
+// });
+
 
 blue.addEventListener('click', e => {
     color.style.backgroundColor = 'blue';
